b8fafe6244f3874a26320e3fe9e3c387a031ecf5
nhuphaNn1602 <nhu.phan@geocomply.com>
2023-05-21 13:35:19 +0700

    Initialize project

---
+++ FoodDeliveryApp/App.js
@@ -0,0 +1,7 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import Navigators from './src/navigators';
+
+export default () => <Navigators />;
+
+// export default Navigator;
