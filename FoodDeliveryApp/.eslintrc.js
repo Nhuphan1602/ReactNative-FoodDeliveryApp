dd2c5b7a25d8b2544c3bb0aec171fb0605a98b2b
rakoon33 <100814861+rakoon33@users.noreply.github.com>
2023-05-22 22:53:59 +0700

    Revert "Add ‘Welcome screen’ and code for feature. Make button to switch conte…"

--- FoodDeliveryApp/.eslintrc.js
+++ FoodDeliveryApp/.eslintrc.js
@@ -1,8 +1,4 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
-  //If you are encountering the "Parsing error: No Babel config file detected" issue specifically in Visual Studio Code with ESLint, it may be necessary to configure the eslint.workingDirectories setting.
-   "eslint.workingDirectories": [
-        {"mode": "auto"}
-    ],
 };
