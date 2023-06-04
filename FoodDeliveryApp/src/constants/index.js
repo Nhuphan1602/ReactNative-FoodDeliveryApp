a1540acf352f32e18c1ce7cd41d77bf92d99ed36
rakoon33 <phamnam7892@gmail.com>
2023-06-05 04:05:48 +0700

    Create PhoneRegisterScreen, Dropdown country code

--- FoodDeliveryApp/src/constants/index.js
+++ FoodDeliveryApp/src/constants/index.js
@@ -2,5 +2,7 @@ import colors from "./colors";
 import images from "./images";
 import fonts from "./fonts";
 import general from "./general";
+import countryCode from "./countryCode";
+import apiConstants from "./apiConstants";
 
-export {colors, images, fonts, general, };
+export {colors, images, fonts, general, countryCode, apiConstants};
