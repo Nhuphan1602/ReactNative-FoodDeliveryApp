53badcb1dc6879610e558001ec6513bc6aa004ee
rakoon33 <phamnam7892@gmail.com>
2023-07-28 20:21:19 +0700

    Add constants to text input in sign up screen

--- FoodDeliveryApp/src/test/SplashScreen.test.js
+++ FoodDeliveryApp/src/test/SplashScreen.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {render} from '@testing-library/react-native';
-import SplashScreen from './SplashScreen';
+import SplashScreen from '../screens/SplashScreen';
 import { colors, images } from '../constants';
 
 describe('SplashScreen', () => {
